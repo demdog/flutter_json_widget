ca1ec30a70838794fe9e24d89fd5619bcb0110ce
_kushal_ <kushal@stylishop.com>
2021-06-30 16:14:45 +0400

    migrated to null safety

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/lyy/Documents/dev_tools/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/lyy/Documents/flutter_ws/flutter_json_widget/example"
+export "FLUTTER_ROOT=/System/Volumes/Data/Files/Application/SDK/flutter2/flutter"
+export "FLUTTER_APPLICATION_PATH=/System/Volumes/Data/Files/Application/MobileApplications/Flutter/flutter_json_widget/example"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/lyy/Documents/dev_tools/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.3"
 export "FLUTTER_BUILD_NUMBER=1.0.3"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
